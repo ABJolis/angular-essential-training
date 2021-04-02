7c9cca92dd980cb933f1fa6d50dc72670394586c
Anne Berg Jolis <annejolis@gmail.com>
2021-04-02 19:00:50 +0100

    start tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [], // <- metadata property used to bring in other angular modules that our module will need
+  imports: [
+    BrowserModule
+  ], // <- metadata property used to bring in other angular modules that our module will need
   declarations: [], // <- property used to make components, directives and pipes available to our module, that don't come from another module
   bootstrap: [] // <-- used for a root module, and lets angular know which component or components will be the starting point for the bootstrap process - basically, the entry point for the bootstrap process
 })
